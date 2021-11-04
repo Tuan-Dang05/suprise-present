ea647932c12218a313062aafe12f2d0ed879e6bd
Đặng Thi Mai Anh <dangthimaianh78@Tuan-Dev.local>
2021-11-04 20:30:13 +0700

    deloy

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10 muộn';
+const titleCard = 'Ib AnhTuandev';
+const contentCard = 'Quên lời chúc rồi :))';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'https://scontent.fhan2-4.fna.fbcdn.net/v/t1.15752-9/248361433_702276867412799_5520070126746960475_n.png?_nc_cat=103&ccb=1-5&_nc_sid=ae9488&_nc_ohc=3fMueLkUAcEAX9qvv9B&_nc_ht=scontent.fhan2-4.fna&oh=0c95bcc4afcee2f2d84329c1e4a0cd53&oe=61A94CC6';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
