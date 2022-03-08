e04ed8bdeebb6f23047f5ad353285495feab9036
tuananh-05 <85606113+tuananh-05@users.noreply.github.com>
2022-03-08 14:39:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ const titleCard = 'Ib AnhTuandev';
 const contentCard = '';
 
 
-const giftImage = 'https://scontent.fhan2-4.fna.fbcdn.net/v/t1.15752-9/248361433_702276867412799_5520070126746960475_n.png?_nc_cat=103&ccb=1-5&_nc_sid=ae9488&_nc_ohc=3fMueLkUAcEAX9qvv9B&_nc_ht=scontent.fhan2-4.fna&oh=0c95bcc4afcee2f2d84329c1e4a0cd53&oe=61A94CC6';
+const giftImage = '';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
