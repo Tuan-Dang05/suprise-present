564d7d92a13c876b5e94b2fba6cb29d727c69a47
tuananh-05 <85606113+tuananh-05@users.noreply.github.com>
2022-03-08 14:29:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Em';
 const giftUrl = 'https://tuananh-05.github.io/my-profile/';
-const eventName = 'Chúc Mừng 20-10 muộn';
+const eventName = 'Chúc cô ngày 20/11';
 const titleCard = 'Ib AnhTuandev';
-const contentCard = 'Quên lời chúc rồi :))';
+const contentCard = '';
 
 
 const giftImage = 'https://scontent.fhan2-4.fna.fbcdn.net/v/t1.15752-9/248361433_702276867412799_5520070126746960475_n.png?_nc_cat=103&ccb=1-5&_nc_sid=ae9488&_nc_ohc=3fMueLkUAcEAX9qvv9B&_nc_ht=scontent.fhan2-4.fna&oh=0c95bcc4afcee2f2d84329c1e4a0cd53&oe=61A94CC6';
